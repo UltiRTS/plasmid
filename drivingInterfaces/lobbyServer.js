8c0ab3c8d01ae9c3607b7d8b017699b47a139f8a
Teresa <teresa@eterea.uk>
2022-01-01 00:51:46 -0500

    zapped again

--- drivingInterfaces/lobbyServer.js
+++ drivingInterfaces/lobbyServer.js
@@ -150,8 +150,8 @@ class LobbyServer {
         if (channelName in this.chats) {
           for (const ppl of this.chats[channelName]) {
             // now let everyone else know
-            this.stateDump(ppl, 'SAYCHAT');
             ppl.state.writeChatMsg({'channelName': channelName, 'author': client.state.username, 'msg': chatMsg});
+            this.stateDump(ppl, 'SAYCHAT');
             // TODO: add autohost msg relay
             // if (this.rooms.hasOwnProperty(channelName)) {
             //   const roomObj = this.rooms[channelName];
